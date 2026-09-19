c7ae3ef7718ba9c3bc515e4c2c102892b35aadfc
fatfatisfat <jameshsieh1116@gmail.com>
2026-09-20 00:08:21 +0800

    20260919

--- C++_code/algorithm_practice/ALGOME1/PD.cpp
+++ C++_code/algorithm_practice/ALGOME1/PD.cpp
@@ -8,7 +8,15 @@ vector<vector<int>> adj;
 vector<vector<int>> dp;
 
 void dfs(int root){
+    dp[root][0] = 0;
+    dp[root][1] = r[root];
+    
+    for (int leaf : adj[root]){
+        dfs(leaf);
 
+        dp[root][0] += max(dp[leaf][0], dp[leaf][1]);
+        dp[root][1] += dp[leaf][0];
+    }
 }
 
 void solve(){
@@ -18,14 +26,14 @@ void solve(){
     dp.assign(n + 1, vector<int>(2, 0));
     vector<bool> has_root (n + 1, false);
 
-    for (int i=0; i<n; i++){
+    for (int i=1; i<=n; i++){
         string name;
         int num;
         cin >> name >> num;
         name_to_id[name] = i;
         r[i] = num;
     }
-    for (int i=0; i<n; i++){
+    for (int i=0; i<n-1; i++){
         string root, leaf;
         cin >> root >> leaf;
         int a = name_to_id[root];
@@ -34,14 +42,16 @@ void solve(){
         has_root[b] = true;
     }
     int root = 1;
-    for (int i=0; i<n; i++){
+    for (int i=1; i<=n; i++){
         if (!has_root[i]){
             root = i;
             break;
         }
     }
 
-    dfs(i);
+    dfs(root);
+
+    cout << max(dp[root][0], dp[root][1]) << "\n";
 }
 
 signed main(){
